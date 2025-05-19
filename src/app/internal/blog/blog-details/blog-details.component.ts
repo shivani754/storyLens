6126251e70faa7a009dcac296ed4feb060e6e6a2
shivani754 <shivanigoyal616@gmail.com>
2025-05-19 15:49:58 +0530

    Created blog details component

---
+++ src/app/internal/blog/blog-details/blog-details.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BlogService } from '../blog.service';
+import { Blog } from '../models/blog.model';
+import { PageLoaderComponent } from '../../../shared/components/page-loader/page-loader.component';
+
+@Component({
+  selector: 'app-blog-details',
+  imports: [PageLoaderComponent],
+  templateUrl: './blog-details.component.html',
+  styleUrl: './blog-details.component.css',
+})
+export class BlogDetailsComponent implements OnInit {
+  postId: number = 0;
+  pageLoader = false;
+  post: Blog = {
+    id: 0,
+    title: '',
+    body: '',
+    userId: 0,
+  };
+  comments: any = [];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private blogService: BlogService,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: any) => {
+      this.postId = +params.postId;
+    });
+    this.getBlogDetails();
+  }
+
+  getBlogDetails() {
+    this.pageLoader = true;
+    const params: any = {
+      postId: this.postId,
+    };
+    this.blogService
+      .getBlogDetails(params)
+      .subscribe((response: any) => {
+        this.post = response;
+        this.getComments();
+      })
+      .add(() => (this.pageLoader = false));
+  }
+
+  getComments() {
+    const params: any = {
+      postId: this.postId,
+    };
+    this.blogService.getComments(params).subscribe((response: any) => {
+      this.comments = response;
+    });
+  }
+
+  goToAddComment() {
+    this.router.navigate(['comment'], {
+      relativeTo: this.route,
+    });
+  }
+}
