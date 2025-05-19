0ba48542ae477dcf27d54f86b1317fa85b243373
shivani754 <shivanigoyal616@gmail.com>
2025-05-19 12:36:49 +0530

    Created internal component and global service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { externalRoutes } from './external/external.routes';
+import { internalRoutes } from './internal/internal.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./external/external.component').then((m) => m.ExternalComponent),
+    children: externalRoutes,
+  },
+  {
+    path: 'internal',
+    loadComponent: () =>
+      import('./internal/internal.component').then((m) => m.InternalComponent),
+    children: internalRoutes,
+  },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+];
