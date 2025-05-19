a5aebc36df9dc7bcef231ed84cb64c30791699c7
shivani754 <shivanigoyal616@gmail.com>
2025-05-19 13:59:19 +0530

    Created page loader component

---
+++ src/app/shared/components/page-loader/page-loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-loader',
+  imports: [],
+  standalone: true,
+  templateUrl: './page-loader.component.html',
+  styleUrl: './page-loader.component.css',
+})
+export class PageLoaderComponent {}
