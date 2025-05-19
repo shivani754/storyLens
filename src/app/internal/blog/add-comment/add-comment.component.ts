678df1daa3a53bdb5c386ee6e15837f868cd657d
shivani754 <shivanigoyal616@gmail.com>
2025-05-19 17:55:12 +0530

    Added add comment component

---
+++ src/app/internal/blog/add-comment/add-comment.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Comment } from '../models/comment.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BlogService } from '../blog.service';
+import { GlobalService } from '../../../core/services/global.service';
+
+@Component({
+  selector: 'app-add-comment',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './add-comment.component.html',
+  styleUrl: './add-comment.component.css',
+})
+export class AddCommentComponent implements OnInit {
+  commentData: Comment = {
+    postId: 0,
+    name: '',
+    body: '',
+  };
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private globalService: GlobalService,
+    private blogService: BlogService,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: any) => {
+      this.commentData.postId = +params.postId;
+    });
+    this.commentData.name = this.globalService.getUserDetails().username;
+  }
+
+  addComment() {
+    this.blogService.addComment(this.commentData).subscribe((response: any) => {
+      this.commentData = response;
+      this.router.navigate(['..'], {
+        relativeTo: this.route,
+        queryParams: {
+          newComment: JSON.stringify(this.commentData),
+        },
+      });
+    });
+  }
+}
