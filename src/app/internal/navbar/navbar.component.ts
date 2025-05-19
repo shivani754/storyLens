a79abe1216b011fae56f57e1d816e222d2efe546
shivani754 <shivanigoyal616@gmail.com>
2025-05-19 12:37:14 +0530

    Created navbar component

---
+++ src/app/internal/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../../core/services/global.service';
+import { UserDetailsModel } from '../../shared/models/user-details.model';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent implements OnInit {
+  isDropdownOpen = false;
+  userDetails: UserDetailsModel = {
+    username: '',
+  };
+
+  constructor(private globalService: GlobalService) {}
+
+  ngOnInit() {
+    this.userDetails = this.globalService.getUserDetails();
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown() {
+    // Delay closing to allow click event on button to trigger first
+    setTimeout(() => {
+      this.isDropdownOpen = false;
+    }, 100);
+  }
+
+  logout() {}
+}
